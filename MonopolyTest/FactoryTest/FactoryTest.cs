ec5dfafc37aa42dee12885686e002befef5f5d1a
Jonasthj <74312201+Jonasthj@users.noreply.github.com>
2021-11-10 10:28:32 +0100

    ShouldReturnPrisonString 🔴

--- MonopolyTest/FactoryTest/FactoryTest.cs
+++ MonopolyTest/FactoryTest/FactoryTest.cs
@@ -6,16 +6,25 @@ namespace MonopolyTest.FactoryTest
     public class FactoryTest
     {
         [Test]
-        public void shouldReturnChanceString()
+        public void ShouldReturnChanceString()
         {
             Chance chance = new();
             chance.Id = 2;
             chance.Name = "Chance";
             chance.Description = "Move three steps back :(";
-            
             StringAssert.Contains(chance.Name, chance.ToString());
             StringAssert.Contains(chance.Description, chance.ToString());
-            //chance.PrintSquare();
         }
+
+        [Test]
+        public void ShouldReturnPrisonString()
+        {
+            Prison prison = new();
+            prison.Id = 2;
+            prison.Name = "Ila fengsel";
+            StringAssert.Contains(prison.Name, prison.ToString());
+        }
+        
+        
     }
 }
