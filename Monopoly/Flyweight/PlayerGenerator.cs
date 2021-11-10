e39e618a2939eac862ad3908722f08fe1b0ad861
attichouse <77102070+attichouse@users.noreply.github.com>
2021-11-10 14:01:27 +0100

    PlayerGenerator.cs

--- Monopoly/Flyweight/PlayerGenerator.cs
+++ Monopoly/Flyweight/PlayerGenerator.cs
@@ -2,6 +2,9 @@ namespace Monopoly.Flyweight
 {
     public class PlayerGenerator
     {
-        
+        public Player Get(int id)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
