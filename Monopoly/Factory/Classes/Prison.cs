78a8d6244c86149b5bed99d5cd78b3a65e2cc6d3
attichouse <77102070+attichouse@users.noreply.github.com>
2021-11-10 10:43:39 +0100

    prisonTest - failing

--- Monopoly/Factory/Classes/Prison.cs
+++ Monopoly/Factory/Classes/Prison.cs
@@ -4,9 +4,19 @@ namespace Monopoly.Factory.Classes
 {
     public class Prison : ISquare
     {
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+
         public void PrintSquare()
         {
             throw new System.NotImplementedException();
+        }
+        
+        public override string ToString()
+        {
+            return "Id: " + Id + "\n" +
+                   "Name: " + Name;
         }
     }
 }
--- MonopolyTest/FactoryTest/FactoryTest.cs
+++ MonopolyTest/FactoryTest/FactoryTest.cs
@@ -1,4 +1,5 @@
-﻿using Monopoly.Factory.Classes;
+﻿using System;
+using Monopoly.Factory.Classes;
 using NUnit.Framework;
 
 namespace MonopolyTest.FactoryTest
@@ -8,23 +9,29 @@ namespace MonopolyTest.FactoryTest
         [Test]
         public void ShouldReturnChanceString()
         {
+            Guid uuid = Guid.NewGuid();
             Chance chance = new();
-            chance.Id = 2;
-            chance.Name = "Chance";
-            chance.Description = "Move three steps back :(";
+            chance.Name = uuid.ToString();
             StringAssert.Contains(chance.Name, chance.ToString());
-            StringAssert.Contains(chance.Description, chance.ToString());
         }
 
         [Test]
         public void ShouldReturnPrisonString()
         {
+            Guid uuid = Guid.NewGuid();
             Prison prison = new();
-            prison.Id = 2;
-            prison.Name = "Ila fengsel";
+            prison.Name = uuid.ToString();
             StringAssert.Contains(prison.Name, prison.ToString());
         }
+
         
-        
+        [Test]
+        public void ShouldReturnPropertyString()
+        {
+            Guid uuid = Guid.NewGuid();
+            Property property = new();
+            property.Name = uuid.ToString();
+            StringAssert.Contains(property.Name , prison.ToString());
+        }  
     }
 }
