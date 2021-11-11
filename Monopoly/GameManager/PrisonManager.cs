56f0aafe3c99d0e5940bd5010510bd137028bd0b
attichouse <77102070+attichouse@users.noreply.github.com>
2021-11-11 11:20:10 +0100

    PrisonManager TODO

--- Monopoly/GameManager/PrisonManager.cs
+++ Monopoly/GameManager/PrisonManager.cs
@@ -1,14 +1,22 @@
 using System;
+using Monopoly.Flyweight;
 
 namespace Monopoly.GameManager
 {
     public class PrisonManager
     {
-        private bool InPrison { get; set; }
+        //TODO Hente spiller-index fra boardmap for å sjekke om spiller er på fengsel-index
+        private bool InPrison { get; }
 
+        public PrisonManager(bool inPrison, Player player)
+        {
+            InPrison = inPrison;
+            
+        }
+        
         public void Punishment()
         {
-            if (InPrison)
+            if (Player.InPrison)
             {
                 Console.WriteLine("You're in prison. You have to skip a round");
             }
