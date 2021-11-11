ce6087d41dcf62d12affd0ade952466893c6765d
attichouse <77102070+attichouse@users.noreply.github.com>
2021-11-11 10:55:57 +0100

    prison manager Punishment setup

--- Monopoly/GameManager/PrisonManager.cs
+++ Monopoly/GameManager/PrisonManager.cs
@@ -1,7 +1,17 @@
+using System;
+
 namespace Monopoly.GameManager
 {
     public class PrisonManager
     {
-        
+        private bool InPrison { get; set; }
+
+        public void Punishment()
+        {
+            if (InPrison)
+            {
+                Console.WriteLine("You're in prison. You have to skip a round");
+            }
+        }
     }
 }
