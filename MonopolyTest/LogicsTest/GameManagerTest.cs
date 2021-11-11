c2d493dc7e7df9f36c77ae34ddf4d5f0eed98273
Martin Teigen <martin@teigen.biz>
2021-11-11 14:37:53 +0100

    Refactor GameManagerTest.cs

--- MonopolyTest/LogicsTest/GameManagerTest.cs
+++ MonopolyTest/LogicsTest/GameManagerTest.cs
@@ -18,6 +18,36 @@ namespace MonopolyTest
             Assert.AreEqual(manager.map.Players[3], 0);
             Assert.AreEqual(manager.map.Players[4], 0);
         }
+
+        [Test]
+        public void ShoulsAddPlayersAndDeleteOne()
+        {
+            // GameManager game = new GameManager();
+            //
+            // //Creating players object on boardmap
+            // int count = Convert.ToInt32(Console.ReadLine());
+            // game.CreatePlayers(count);
+            //
+            // //Setting players values 
+            // for (int i = 1; i < count+1; i++)
+            // {
+            //     string name = Console.ReadLine();
+            //     game.FillPlayersInfo(name, i);
+            // }
+            // foreach (KeyValuePair<int, Player> player in PlayerGenerator.Players)
+            // {
+            //     Console.WriteLine("Key: {0}, {1}", 
+            //         player.Key, player.Value);
+            // }
+            //
+            // PlayerGenerator.Delete(1);
+            //
+            // foreach (KeyValuePair<int, Player> player in PlayerGenerator.Players)
+            // {
+            //     Console.WriteLine("Key: {0}, {1}", 
+            //         player.Key, player.Value);
+            // }
+        }
         
         
     }
